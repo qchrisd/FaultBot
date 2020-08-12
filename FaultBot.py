5abcac70e128bf5d85ec98b552b7b64b7371dcb9
qchrisd <q.chris.d@gmail.com>
2020-08-12 17:48:39 -0400

    Initial commit

---
+++ FaultBot.py
@@ -0,0 +1,194 @@
+# The fault bot that will upload fault data to the discord
+
+# Import the dependencies
+import os
+import discord
+from dotenv import load_dotenv
+import FaultAPI as f
+import FaultFormat as fformat
+
+# Load the environmental variables from the .env file
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+# Create the discord client
+client = discord.Client()
+
+# Help text for the bot
+helpMessage = """**Welcome to the Support Squad fault bot!**
+I'm still under development so please be patient with me.
+
+I can help with the following things:
+
+**!help** - gives you this chat. You knew this one already!
+
+**!matches _<username> <n matches>_** - gives you the last n matches for the specified fault user. I don't know how many matches discord will allow me to send but I should be good for at least 2!
+
+**!heroes _<username> <sort>_** - gives you the hero information for the specified user sorted.
+You may sort by the following criteria:
+- games
+- kills
+- deaths
+- assists
+- name
+
+**!elo _<username>_** - Gets the specified player's MMR information.
+
+More functionality is on the way!
+"""
+
+## Utility methods
+def isString(datum):
+    if type(datum) == str:
+        return True
+    else:
+        return False
+
+def isInt(datum):
+    if type(datum) == int:
+        return True
+    else:
+        return False
+
+
+# The on ready event for when the bot logs into the server
+@client.event
+async def on_ready():
+        print('{0} has successfully connected to Discord'.format(client.user))
+
+# Sends hero data to the discord
+async def sendHeroes(message, messageParts):
+
+    """Sends the hero usage statistics to the discord channel.
+    Currently sorting by "games". Other sort criteria include:
+     - wins
+     - games
+     - kills
+     - deaths
+     - assists
+    """
+
+    print('Hero data requested')
+
+    # Sets default sortBy criteria
+    sortBy = 'games'
+    username = str(message.author).split('#')[0]
+
+    # 1 param given, should be a name
+    if len(messageParts) == 2:
+        # TODO need to add type checking
+        if isString(messageParts[1]):
+            username = messageParts[1]
+        else:
+            await message.channel.send("The player name you entered was not a string. Please try again.")
+            return
+    # 2 params given, should be a name and sort criteria
+    elif len(messageParts) > 2:
+        if isString(messageParts[1]):
+            username = messageParts[1]
+        else:
+            await message.channel.send("The player name you entered was not a string. Please try again.")
+            return
+        sortBy = messageParts[2]
+    
+    # Get Player ID
+    player = f.get_id(username)
+    # Get the hero data
+    heroes = f.get_heroes(player)
+    # Sort the hero data
+    output = fformat.format_heroes(heroes, sortBy, username)
+    # send it to the channel
+    await message.channel.send(output)
+    return
+
+# Sends match info
+# TODO fix the type checking for 3 params
+async def sendMatches(message, messageParts):
+    matches = 'No matches found'
+    # No params given
+    if len(messageParts) == 1:
+        print('no params entered')
+        matches = f.get_matches(f.get_id(str(message.author).split('#')[0]), 1)
+    # 1 param given, should be a name
+    elif len(messageParts) == 2:
+        # TODO need to add type checking
+        print('1 param entered')
+        if isString(messageParts[1]):
+            matches = f.get_matches(f.get_id(messageParts[1]), 1)
+        else:
+            await message.channel.send("The player name you entered was not a string. Please try again.")
+            return
+    # 2 or more params given, should be a name and number
+    else:
+        # TODO need to add type checking amd limit to 3
+        print('2 params entered')
+        if int(messageParts[2]) > 3:
+            messageParts[2] = 3
+        matches = f.get_matches(f.get_id(messageParts[1]), messageParts[2])           
+
+    # Sends message to the discord
+    await message.channel.send(fformat.format_matches(matches))
+    return
+
+
+# Sends the elo information of the given player
+async def send_elo(message, messageParts):
+
+    print('ELO data requested')
+
+    # Message author is the user
+    user = str(message.author).split('#')[0]
+
+    # If user is specified in command
+    if len(messageParts) >= 2:
+        user = messageParts[1]
+
+    # Gets the user ELO information
+    id = f.get_id(user)
+    player = f.get_elo(id)
+    output = fformat.format_elo(player)
+
+    # Send message
+    await(message.channel.send(output))
+    
+
+# Test the embed message functionality of discord
+async def send_embed(message, messageParts):
+    pass
+
+
+# The message event that runs when a message is sent to the server
+@client.event
+async def on_message(message):
+    # prevents running event for self posts
+    if message.author == client.user:
+        return
+
+    # Split the message into parts
+    messageParts = message.content.split()
+
+    # Send help message
+    if messageParts[0] == '!help':
+        await message.channel.send(helpMessage)
+        return
+
+    if messageParts[0] == '!heroes':
+        await sendHeroes(message, messageParts)
+
+    # If the message is requesting matches
+    if messageParts[0] == '!matches':
+        await sendMatches(message, messageParts)
+
+    # Message requests elo information
+    if messageParts[0] == '!elo':
+        await send_elo(message, messageParts)
+
+    # Message embed tests
+    if messageParts[0] == '!embed':
+        await send_embed(message, messageParts)
+
+# Runs the client
+client.run(TOKEN)
+
+
---
+++ FaultFormat.py
@@ -0,0 +1,114 @@
+'''
+This program deals with all the formatting of the fault API.
+
+Currently the functionality allows for the following:
+- Formatting matches
+- Formatting hero stats
+
+To Do:
+- Refactor code to by pythonic
+- Add type checking and error handling
+
+'''
+
+
+# Import Fault API for hero and item imports
+import FaultAPI as f
+
+
+# Format and return the matches requested using getMatches()
+# Returns a string
+def format_matches(matches):
+    # Checks for empty match record
+    if matches == []:
+        return('No matches found for that player ID')
+
+    # create the output string
+    output = ""
+
+    # Iterates through each match
+    for i, match in enumerate(matches):
+        # Inits some useful vars when formatting
+        team0 = []
+        team1 = []
+        AveELO0 = 0
+        AveELO1 = 0
+
+        # Adds the match length and match ID
+        output += '\n\nLength: {0} [ID: {1}]\n'.format(match['TimeLength'], str(match['ID']))
+        
+        # Iterates through players in the match
+        for player in match['players']:
+            # Gets the player MMR information
+            player_elo = f.get_elo(player['PlayerID'])
+            # Creates a string to add to the output
+            player_string = '\n - {0} ({1}: {2} ELO, Rank {3})'.format(f.heroes[player['HeroID']], player['Username'], player_elo['MMR'], player_elo['ranking'])
+            # Sorts players into teams and adds ELO
+            if player['Team'] == 0:
+                team0.append(player_string)
+                AveELO0 += player_elo['MMR']
+            else:
+                team1.append(player_string)
+                AveELO1 += player_elo['MMR']
+        
+        # Averages ELO across 5 players
+        AveELO0 = int(AveELO0/5)
+        AveELO1 = int(AveELO1/5)
+
+        # Adds the teams to output with headers
+        output += 'Team 1 ({0} Average ELO):'.format(AveELO0)
+        for player in team0:
+            output += player
+        output += '\n\nTeam 2 ({0} Average ELO):'.format(AveELO1)
+        for player in team1:
+            output += player
+
+        # adds a seperator for multiple matches
+        if i < len(matches)-1:
+            output += '\n\n-------------'
+        
+    # return output string
+    return output
+
+
+# Format player hero preferences
+def format_heroes(heroList, sortCriteria, username):
+    # TODO add sortCriteria checkign
+
+    # Sort the vi
+    heroSorted = sorted(heroList, key=lambda item: item[sortCriteria], reverse=True)
+
+    # output string with the header
+    output = "Top Hero Statistics for {0} (Sorted by {1})\n".format(username, sortCriteria)
+
+    # Create each hero line and add it to the output string
+    for hero in heroSorted:
+        winrate = hero['wins']/hero['games']*100
+        totKDA = (hero['kills']+hero['assists'])/hero['deaths']
+        heroLine = " - {0} ({1} games): {2:.2f}% ({3:.2f} total KDA)\n".format(hero['hero'], hero['games'], winrate, totKDA)
+        output += heroLine
+
+    return output
+
+
+# Format a player elo for discord
+def format_elo(player):
+
+    # Create output string
+    output = ""
+
+    # Check if user is bad
+    if player == []:
+        output = "No player found"
+        return output
+
+    # Add player name
+    output += "{0}\n".format(player['username'])
+
+    # Add MMR information
+    output += "{0} {1} (Rank {2})".format(player['MMR'], player['eloTitle'], player['ranking'])
+
+    return output
+
+
+
