88929ab68afa39413a92f598df4bc005e3f0a56f
qchrisd <36607765+qchrisd@users.noreply.github.com>
2022-03-01 20:25:40 -0500

    Created constants file

---
+++ constants.py
@@ -0,0 +1,28 @@
+"""
+Contains constants for the discord bot.
+
+"""
+
+# Bot Messages
+# Help text for the bot
+helpMessage = """**Welcome to the Support Squad fault bot!**
+I'm still under development so please be patient with me.
+
+I can help with the following things:
+
+**!help** - gives you this chat. You knew this one already!
+
+**!matches _<username> <n matches>_** - gives you the last n matches for the specified fault user. I don't know how many matches discord will allow me to send but I should be good for at least 2!
+
+**!heroes _<username> <sort>_** - gives you the hero information for the specified user sorted.
+You may sort by the following criteria:
+- games
+- kills
+- deaths
+- assists
+- name
+
+**!elo _<username>_** - Gets the specified player's MMR information.
+
+More functionality is on the way!
+"""
