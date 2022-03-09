a975a9b9e80da567830b7f97e6733ceb77fce39d
qchrisd <q.chris.d@gmail.com>
2022-03-09 17:21:08 -0500

    Adds test for get_from_dict()

--- tests/test_cog_helpers.py
+++ tests/test_cog_helpers.py
@@ -3,7 +3,9 @@ Contains unit tests for the CommandFunctions module.
 """
 
 import unittest
-from bot.cog_helpers import update_dict, remove_from_dict
+from bot.cog_helpers import (update_dict, 
+                             remove_from_dict,
+                             get_from_dict)
 
 
 class TestCommandFunctions(unittest.TestCase):
@@ -16,6 +18,14 @@ class TestCommandFunctions(unittest.TestCase):
     def test_remove_from_dict(self):
         actual = remove_from_dict(users_dict = {"guild":{123:{"qchrisd#1644":"qchrisd"}}}, guild_id=123, discord_name="qchrisd#1644")
         self.assertEqual(actual, {"guild":{123:{}}})
+
+
+    def test_get_from_dict(self):
+        users_dict = {"guild":{123:{"qchrisd#1644":"qchrisd"}}}
+        actual = get_from_dict(users_dict, guild_id=123, discord_name= "qchrisd#1644")
+        self.assertEqual(actual, "qchrisd")
+
+
 
 if __name__ == '__main__':
     unittest.main()
