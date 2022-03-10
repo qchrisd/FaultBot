4ba79718aa51620d4ee49a71838697f33d50c73f
qchrisd <q.chris.d@gmail.com>
2022-03-10 13:12:32 -0500

    Updated logging

--- tests/test_cog_helpers.py
+++ tests/test_cog_helpers.py
@@ -3,12 +3,19 @@ Contains unit tests for the CommandFunctions module.
 """
 
 import unittest
+import unittest.mock as mock
 from bot.cog_helpers import (update_dict, 
                              remove_from_dict,
                              get_from_dict)
 
 
-class TestCommandFunctions(unittest.TestCase):
+class TestCogHelpers(unittest.TestCase):
+
+
+    @mock.patch(open)
+    def test_read_file(self, mock_open):
+        pass
+
 
     def test_update_dict(self):
         actual = update_dict(users_dict = {"guild":{}}, guild_id=123, discord_name="qchrisd#1644", fault_name="qchrisd")
